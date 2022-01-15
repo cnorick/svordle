af2a377747a93eb66127a46547879564098d1824
Nathan Orick <cnathanorick@gmail.com>
2022-01-15 10:06:04 -0500

    Make words more common

--- scripts/dictionaryParser.js
+++ scripts/dictionaryParser.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const readline = require('readline');
 
-get5LetterWords();
+// get5LetterWords();
+// getRanked5LetterWords();
+getMostCommon5LetterWords();
+
+
 async function get5LetterWords() {
     const fiveLetterWords = [];
     const fileStream = fs.createReadStream(`${__dirname}/allWords.txt`);
@@ -14,4 +18,39 @@ async function get5LetterWords() {
     }
 
     fs.writeFileSync(`${__dirname}/fiveLetterWords.json`, JSON.stringify(fiveLetterWords));
+}
+
+async function getRanked5LetterWords() {
+    const fiveLetterWords = [];
+    const fileStream = fs.createReadStream(`${__dirname}/wordsByUsage.txt`);
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+    for await (const line of rl ) {
+        const commaPos = line.indexOf(',');
+        word = line.substring(1, commaPos);
+        word.length === 5 && fiveLetterWords.push(word);
+    }
+
+    fs.writeFileSync(`${__dirname}/rankedFiveLetterWords.json`, JSON.stringify(fiveLetterWords));
+}
+
+async function getMostCommon5LetterWords() {
+    const fiveLetterWords = [];
+    const fileStream = fs.createReadStream(`${__dirname}/wordsByUsage.txt`);
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+    for await (const line of rl ) {
+        const commaPos = line.indexOf(',');
+        word = line.substring(1, commaPos);
+        word.length === 5 && fiveLetterWords.push(word);
+    }
+
+    console.log(fiveLetterWords.length);
+    console.log(fiveLetterWords[4000])
+
+    fs.writeFileSync(`${__dirname}/rankedFiveLetterWords.json`, JSON.stringify(fiveLetterWords.slice(0, 4000)));
 }
--- src/wordCreator.js
+++ src/wordCreator.js
@@ -1,7 +1,10 @@
 import * as dictionaryModule from './dictionary.json';
+import { rbeta } from './betaDistribution';
+
 const dictionary = dictionaryModule.default;
 export function getRandomWord (){
-    return dictionary[Math.floor(Math.random() * dictionary.length)];
+    const randomNum = rbeta(2, 5);
+    return dictionary[Math.floor(randomNum * dictionary.length)];
 }
 export function isWordInDictionary(word) {
     return dictionary.includes(word);
