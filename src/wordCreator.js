848dff2622ee19465688897d04f054b9e64589d1
Nathan Orick <cnathanorick@gmail.com>
2022-01-16 18:58:59 -0500

    Fix getRandomWord

--- src/wordCreator.js
+++ src/wordCreator.js
@@ -5,7 +5,7 @@ const dictionary = dictionaryModule.default;
 export function getRandomWord (){
     const smallerDictionary = dictionary.slice(0, 4000);
     const randomNum = rbeta(2, 5);
-    return smallerDictionary[Math.floor(randomNum * dictionary.length)];
+    return smallerDictionary[Math.floor(randomNum * smallerDictionary.length)];
 }
 export function isWordInDictionary(word) {
     return dictionary.includes(word);
